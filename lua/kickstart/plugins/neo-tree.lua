48ef79ee9ed91dee474aaf5b6efcf6ecbdeeea86
Rasheed19 <ibraheem.abdulrasheed@gmail.com>
2025-08-14 12:14:13 +0100

    feat: config lsp and more

---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,30 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+      filtered_items = {
+        visible = true, -- Show filtered (hidden) items
+        hide_dotfiles = false, -- Do NOT hide dotfiles (e.g., .git, .env)
+        hide_gitignored = false, -- Optional: show gitignored files too
+      },
+    },
+  },
+}
